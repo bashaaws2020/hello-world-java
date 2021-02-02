0a8270fcda82b81c6356b7b441294ce88bf389ca
Basha <bashaaws2020@gmail.com>
2021-02-02 14:46:59 +0000

    add poll scm lines for testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-    System.out.println("Hello1, World");
+    System.out.println("Hello1, India");
     System.out.println("Testing, pollscm");
     System.out.println("Build, Automation1");
     
